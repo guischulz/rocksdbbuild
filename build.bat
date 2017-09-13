1fc41ebeb53ac97880378edf1b3391215aad3d3c
Guido Schulz <guido.schulz@mettenmeier.de>
2017-09-13 22:12:18 +0200

    Add JEMALLOC; use WinSDK 8.1 and Toolset v140 for Windows 7 compatibility

--- build.bat
+++ build.bat
@@ -1,46 +1,53 @@
 @setlocal
 call %~dp0environment.bat
+pushd %ROCKSDB_DIR%
 
 if exist "%THIRDPARTY_HOME%\gflags-%GFLAGS_VERSION%" rd /s/q "%THIRDPARTY_HOME%\gflags-%GFLAGS_VERSION%"
 if exist "%THIRDPARTY_HOME%\snappy-%SNAPPY_VERSION%" rd /s/q "%THIRDPARTY_HOME%\snappy-%SNAPPY_VERSION%"
 if exist "%THIRDPARTY_HOME%\lz4-%LZ4_VERSION%" rd /s/q "%THIRDPARTY_HOME%\lz4-%LZ4_VERSION%"
 if exist "%THIRDPARTY_HOME%\zlib-%ZLIB_VERSION%" rd /s/q "%THIRDPARTY_HOME%\zlib-%ZLIB_VERSION%"
+if exist "%THIRDPARTY_HOME%\jemalloc-cmake-jemalloc-cmake.%JEMALLOC_VERSION%"  rd /s/q "%THIRDPARTY_HOME%\jemalloc-cmake-jemalloc-cmake.%JEMALLOC_VERSION%"
 
-%UNZIP_EXE% -o %THIRDPARTY_PACKAGES_DIR%\gflags-%GFLAGS_VERSION%.zip -d %THIRDPARTY_HOME%
-%UNZIP_EXE% -o %THIRDPARTY_PACKAGES_DIR%\snappy-%SNAPPY_VERSION%.zip -d %THIRDPARTY_HOME%
-%UNZIP_EXE% -o %THIRDPARTY_PACKAGES_DIR%\lz4-%LZ4_VERSION%.zip -d %THIRDPARTY_HOME%
-%UNZIP_EXE% -o %THIRDPARTY_PACKAGES_DIR%\zlib-%ZLIB_VERSION%.zip -d %THIRDPARTY_HOME%
-
-pushd %ROCKSDB_DIR%
 git clean -xdf
 git checkout .
 git fetch
 git checkout tags/%ROCKSDB_VERSION%
 
+%UNZIP_EXE% -o %THIRDPARTY_PACKAGES_DIR%\gflags-%GFLAGS_VERSION%.zip -d %THIRDPARTY_HOME%
+%UNZIP_EXE% -o %THIRDPARTY_PACKAGES_DIR%\snappy-%SNAPPY_VERSION%.zip -d %THIRDPARTY_HOME%
+%UNZIP_EXE% -o %THIRDPARTY_PACKAGES_DIR%\lz4-%LZ4_VERSION%.zip -d %THIRDPARTY_HOME%
+%UNZIP_EXE% -o %THIRDPARTY_PACKAGES_DIR%\zlib-%ZLIB_VERSION%.zip -d %THIRDPARTY_HOME%
+%UNZIP_EXE% -o "%THIRDPARTY_PACKAGES_DIR%\jemalloc-cmake-jemalloc-cmake.%JEMALLOC_VERSION%.zip" -d %THIRDPARTY_HOME%
+
 xcopy /e/s/v/y %~dp0mods_%ROCKSDB_VERSION% %ROCKSDB_DIR%
 
 pushd %THIRDPARTY_HOME%\gflags-%GFLAGS_VERSION%
-cmake -G "Visual Studio 14 Win64" -T v140
-msbuild gflags.sln /property:PlatformToolset=v140 /p:Configuration=Release /p:Platform=x64
+cmake -G %CMAKE_GENERATOR_PLATFORM% -T %PLATFORM_TOOLSET% -DCMAKE_SYSTEM_VERSION=%CMAKE_SYSTEM_VERSION%
+msbuild gflags.sln /property:PlatformToolset=%PLATFORM_TOOLSET% /p:Configuration=Release /p:Platform=x64
 popd
 
 pushd %THIRDPARTY_HOME%\snappy-%SNAPPY_VERSION%
-cmake -G "Visual Studio 14 Win64" -T v140
-msbuild snappy.sln /property:PlatformToolset=v140 /p:Configuration=Release /p:Platform=x64
+cmake -G %CMAKE_GENERATOR_PLATFORM% -T %PLATFORM_TOOLSET% -DCMAKE_SYSTEM_VERSION=%CMAKE_SYSTEM_VERSION%
+msbuild snappy.sln /property:PlatformToolset=%PLATFORM_TOOLSET% /p:Configuration=Release /p:Platform=x64
 popd
 
 pushd %THIRDPARTY_HOME%\lz4-%LZ4_VERSION%\visual\VS2010
 devenv lz4.sln /upgrade
-msbuild lz4.sln /property:PlatformToolset=v140 /p:Configuration=Release /p:Platform=x64
+msbuild lz4.sln /property:PlatformToolset=%PLATFORM_TOOLSET% /p:Configuration=Release /p:Platform=x64 /p:TargetPlatformVersion=%WINSDK_VERSION%
 popd
 
 pushd %THIRDPARTY_HOME%\zlib-%ZLIB_VERSION%
-cmake -G "Visual Studio 14 Win64" -T v140
-msbuild zlib.sln /property:PlatformToolset=v140 /p:Configuration=Release /p:Platform=x64
+cmake -G %CMAKE_GENERATOR_PLATFORM% -T %PLATFORM_TOOLSET% -DCMAKE_SYSTEM_VERSION=%CMAKE_SYSTEM_VERSION%
+msbuild zlib.sln /property:PlatformToolset=%PLATFORM_TOOLSET% /p:Configuration=Release /p:Platform=x64
 popd
 
-rem cmake -G "Visual Studio 14 Win64" -DJNI=1 -DGFLAGS=1 -DSNAPPY=1 -DLZ4=1 -DZLIB=1 -DXPRESS=1 -DCMAKE_SYSTEM_VERSION=6.1 -T v140
-cmake -G "Visual Studio 14 Win64" -DJNI=1 -DGFLAGS=1 -DSNAPPY=1 -DLZ4=1 -DZLIB=1 -DXPRESS=1 -T v140
-msbuild rocksdb.sln /property:PlatformToolset=v140 /p:Configuration=Release /p:Platform=x64
+pushd %THIRDPARTY_HOME%\jemalloc-cmake-jemalloc-cmake.%JEMALLOC_VERSION%
+cmake -G %CMAKE_GENERATOR_PLATFORM% -T %PLATFORM_TOOLSET% -DCMAKE_SYSTEM_VERSION=%CMAKE_SYSTEM_VERSION%
+msbuild jemalloc.sln /property:PlatformToolset=%PLATFORM_TOOLSET% /p:Configuration=Release /p:Platform=x64
+popd
+
+rem cmake -G %CMAKE_GENERATOR_PLATFORM% -DJNI=1 -DGFLAGS=1 -DSNAPPY=1 -DLZ4=1 -DZLIB=1 -DXPRESS=1 -DCMAKE_SYSTEM_VERSION=6.1 -T v140
+cmake -G %CMAKE_GENERATOR_PLATFORM% -DJNI=1 -DGFLAGS=1 -DSNAPPY=1 -DLZ4=1 -DZLIB=1 -DXPRESS=1 -DJEMALLOC=1 -T %PLATFORM_TOOLSET% -DCMAKE_SYSTEM_VERSION=%CMAKE_SYSTEM_VERSION%
+msbuild rocksdb.sln /property:PlatformToolset=%PLATFORM_TOOLSET% /p:Configuration=Release /p:Platform=x64
 
 popd
--- environment.bat
+++ environment.bat
@@ -1,22 +1,28 @@
-set CMAKE_HOME=D:\QGIS_Dev\cmake-3.9.2-win64-x64
-set GIT_HOME=C:\Program Files\Git
-set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_79
-set UNZIP_EXE="D:\Tools\unz600xn\unzip.exe"
+set CMAKE_HOME=C:\Tools\CMake
+set GIT_HOME=C:\Tools\Git
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_144
+set UNZIP_EXE="C:\Tools\Unzip\unzip.exe"
 
-set PATH=C:\Windows;C:\Windows\System32;C:\Windows\System32\Wbem;%JAVA_HOME%\bin;%CMAKE_HOME%\bin;%GIT_HOME%\bin
+set PATH=C:\Windows;C:\Windows\System32;C:\Windows\System32\Wbem;C:\windows\System32\WindowsPowerShell\v1.0
+set PATH=%PATH%;%JAVA_HOME%\bin;%CMAKE_HOME%\bin;%GIT_HOME%\bin
 
-rem Visual Studio 2017 C++ build tools using Windows SDK 8.1
-rem call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 8.1
+rem Visual Studio 2017 C++ build tools
+rem set WINSDK_VERSION=10.0.15063.0
+set WINSDK_VERSION=8.1
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 %WINSDK_VERSION%
 
-rem Visual Studio 2017 C++ build tools using Windows SDK 10.0.14393.0
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 10.0.14393.0
-
-set ROCKSDB_DIR=D:\Work\test\rocksdb
+set ROCKSDB_DIR=D:\Work\rocksdb
 set ROCKSDB_VERSION=v5.7.2
-set THIRDPARTY_PACKAGES_DIR=D:\QGIS_Dev\packages
-set THIRDPARTY_HOME=%ROCKSDBDIR%\third-party
+set THIRDPARTY_PACKAGES_DIR=D:\work\packages
+set THIRDPARTY_HOME=%ROCKSDB_DIR%\third-party
 
 set GFLAGS_VERSION=2.2.1
 set SNAPPY_VERSION=1.1.7
 set LZ4_VERSION=1.8.0
 set ZLIB_VERSION=1.2.11
+set JEMALLOC_VERSION=4.3.1
+
+rem set PLATFORM_TOOLSET=v141
+set PLATFORM_TOOLSET=v140
+set CMAKE_GENERATOR_PLATFORM="Visual Studio 15 2017 Win64"
+set CMAKE_SYSTEM_VERSION=%WINSDK_VERSION%
